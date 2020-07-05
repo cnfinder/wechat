c71dcea41771a5b61c1127330792f9a619d2cb6e
finder <171721626@qq.com>
2020-07-05 17:15:02 +0800

    把live 加入到 miniprogram.go中获取

--- miniprogram/miniprogram.go
+++ miniprogram/miniprogram.go
@@ -2,6 +2,7 @@ package miniprogram
 
 import (
 	"github.com/cnfinder/wechat/context"
+	"github.com/cnfinder/wechat/miniprogram/live"
 )
 
 // MiniProgram struct extends context
@@ -14,4 +15,16 @@ func NewMiniProgram(context *context.Context) *MiniProgram {
 	miniProgram := new(MiniProgram)
 	miniProgram.Context = context
 	return miniProgram
+}
+
+
+// 获取直播间实列
+func (this *MiniProgram) GetLiveRoom() *live.LiveRoom{
+	return live.NewLiveRoom(this.Context)
+}
+
+
+// 获取直播商品管理实列
+func (this *MiniProgram) GetLiveGoodsMgr() *live.LiveGoodsMgr{
+	return live.NewLiveGoodsMgr(this.Context)
 }
