5581d85a34fcc46ab82ce815e2772bc38f4c523f
TheWholePerson <HaiChengJ@163.com>
2020-11-16 11:53:04 +0800

    去除输出

--- util/http.go
+++ util/http.go
@@ -181,8 +181,7 @@ func PostXML(uri string, obj interface{}) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	xmlData_str := fmt.Sprintf("%s " , xmlData)
-	_ = xmlData_str
+
 	body := bytes.NewBuffer(xmlData)
 	response, err := http.Post(uri, "application/xml;charset=utf-8", body)
 	if err != nil {
